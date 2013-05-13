6d98c7e39ec3dea1866296fdc32d99fde1d148e7
Kenneth <github@kennethcalamay.com>
2013-05-13 10:13:17 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onelife::Application.config.secret_token = '1e3cf3e229a8dc10cf109c9d237702f90eef4f9128e89623a9c446ce53cf86887653f8c235a887403d5f847a2f424d814fc0f915b4a8aeae4e7618486816987d'
